56ab2617063e17fa3f480d41c29d4de22dd659b5
Ammon <ammon-mino@web.de>
2017-11-02 11:03:41 +0100

    add showAttributes

--- org.dieschnittstelle.jee.esa.bas/src/main/java/org/dieschnittstelle/jee/esa/basics/ShowAnnotations.java
+++ org.dieschnittstelle.jee.esa.bas/src/main/java/org/dieschnittstelle/jee/esa/basics/ShowAnnotations.java
@@ -4,6 +4,9 @@ package org.dieschnittstelle.jee.esa.basics;
 import org.dieschnittstelle.jee.esa.basics.annotations.AnnotatedStockItemBuilder;
 import org.dieschnittstelle.jee.esa.basics.annotations.StockItemProxyImpl;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
 import static org.dieschnittstelle.jee.esa.utils.Utils.*;
 
 public class ShowAnnotations {
@@ -21,16 +24,33 @@ public class ShowAnnotations {
 		}
 
 		// we initialise a consumer
-		Consumer consumer = new Consumer();
+		//Consumer consumer = new Consumer();
 		// ... and let them consume
-		consumer.doShopping(collection.getStockItems());
+		//consumer.doShopping(collection.getStockItems());
 	}
 
 	/*
-	 * UE BAS2 
+	 * UE BAS2
 	 */
 	private static void showAttributes(Object consumable) {
-		show("class is: " + consumable.getClass());
+		String message = "{ "+consumable.getClass().getSimpleName();
+		for(Field field:consumable.getClass().getDeclaredFields()){
+			message+= " "+field.getName()+":";
+			try {
+				message += consumable.getClass().getMethod(
+						"get"+
+								Character.toUpperCase(field.getName().charAt(0))+
+								field.getName().substring(1)
+				).invoke(consumable)+",";
+			} catch(NoSuchMethodException exc) {
+				show(exc.getMessage());
+			} catch (IllegalAccessException exc) {
+				show(exc.getMessage());
+			} catch (InvocationTargetException exc) {
+				show(exc.getMessage());
+			}
+		}
+		show(message.substring(0, message.length() - 1)+"}");
 	}
 
 }
