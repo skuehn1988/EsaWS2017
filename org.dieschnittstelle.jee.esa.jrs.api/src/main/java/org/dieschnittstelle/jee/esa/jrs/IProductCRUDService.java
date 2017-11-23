cfa4f261a1df8ef46f2509188e11be39dadc8907
sebastian <sebastiankuehn1988@gmail.com>
2017-11-23 12:21:16 +0100

    First Commit

--- org.dieschnittstelle.jee.esa.jrs.api/src/main/java/org/dieschnittstelle/jee/esa/jrs/IProductCRUDService.java
+++ org.dieschnittstelle.jee.esa.jrs.api/src/main/java/org/dieschnittstelle/jee/esa/jrs/IProductCRUDService.java
@@ -1,11 +1,21 @@
 package org.dieschnittstelle.jee.esa.jrs;
 
+import org.dieschnittstelle.jee.esa.entities.erp.AbstractProduct;
 import org.dieschnittstelle.jee.esa.entities.erp.IndividualisedProductItem;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.util.List;
 
+@Path("/product")
+@Consumes({MediaType.APPLICATION_JSON})
+@Produces({MediaType.APPLICATION_JSON})
+
+
+public interface IProductCRUDService {
+
 /*
- * UE JRS2: 
+ * UE JRS2:
  * deklarieren Sie hier Methoden fuer:
  * - die Erstellung eines Produkts
  * - das Auslesen aller Produkte
@@ -15,9 +25,32 @@ import java.util.List;
  * und machen Sie diese Methoden mittels JAX-RS Annotationen als WebService verfuegbar
  */
 
+	@GET
+	List<AbstractProduct> readAllProducts();
+
+	@GET
+	@Path("/{productId}") // path spezifiziert die URI genauer, ID wird übermittelt an methode
+	AbstractProduct readProduct(@PathParam("productId") long id); //pathparam nimmt UIR id auf
+
+	@POST
+		// für create methode, nicht über uri, weil komplexer datentyp, also im Body mit JASON
+	AbstractProduct createProduct(AbstractProduct product);
+
+	@PUT
+	@Path("/{productId}")
+	AbstractProduct updateProduct(@PathParam("productId") long id, AbstractProduct product);
+
+	@DELETE //auf Serverseite wird ein longwert geparsed aus der ID in der URI
+	@Path("/{productId}")
+	boolean deleteProduct(@PathParam("productId") long id);
+
+
+}
+
+
 /*
  * UE JRS3: aendern Sie Argument- und Rueckgabetypen der Methoden von IndividualisedProductItem auf AbstractProduct
- */
+
 public interface IProductCRUDService {
 
 	public IndividualisedProductItem createProduct(IndividualisedProductItem prod);
@@ -32,3 +65,5 @@ public interface IProductCRUDService {
 	public IndividualisedProductItem readProduct(long id);
 			
 }
+*/
+
